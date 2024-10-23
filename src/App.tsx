982edc6d45608e1bc4185a839d4e12b122b9cdc9
vanessa <carloscrus0901@gmail.com>
2024-10-22 20:03:17 -0400

    cofigurando firebase

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,21 @@ import videoSample from "./assets/video/video.mp4"
 import styles from "./App.module.css"
 
 const App = () =>{
-     return(
-        <div className={styles.container}>
-            <h1 className={styles.title}>Nature</h1>
+     return (
+       <div className={styles.container}>
+         <h1 className={styles.title}>Nature</h1>
 
-            <img src={img01} alt="arvores" />
-            <p className={styles.label}>Arvores</p>
+         <img src={img01} alt="arvores" />
+         <p className={styles.label}>Arvores</p>
 
-            <img src={img02} alt="arvores com montanha" />
-            <p  className={styles.label}>Árvores com montanha</p>
+         <img src={img02} alt="arvores com montanha" />
+         <p className={styles.label}>Árvores com montanha</p>
 
-            <video autoPlay={true}>
-                <source src={videoSample} type="video/mp4"/>
-            </video>
-        </div>
-     )
+         <video autoPlay={true} loop={true} controls={true}>
+           <source src={videoSample} type="video/mp4" />
+         </video>
+         <p className={styles.label}> Um vídeo da natureza</p>
+       </div>
+     );
 };
  export default App;
